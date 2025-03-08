bd7b23e5822348efdab4670a6e11f7f0383b1ad0
Christian <minisirhc@gmail.com>
2025-03-07 20:49:10 -0600

    [add] consumer

--- src/notification/notification.go
+++ src/notification/notification.go
@@ -1,2 +1,21 @@
 package notification
 
+import (
+	"bytes"
+	"log"
+	"net/http"
+)
+
+
+func SendNotification(notificationAPIURL string, message string) {
+	jsonData := []byte(`{"message": "` + message + `"}`)
+
+	resp, err := http.Post(notificationAPIURL, "application/json", bytes.NewBuffer(jsonData))
+	if err != nil {
+		log.Printf("Error sending notification to API: %s", err)
+		return
+	}
+	defer resp.Body.Close()
+
+	log.Printf("Notification sent, received status: %s", resp.Status)
+}
